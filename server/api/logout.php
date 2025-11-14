ef4ac88514a2044bd14619194e540f235f54da65
cuinoop1610 <118621362+cuinoop1610@users.noreply.github.com>
2025-11-14 13:33:10 +0700

    Update logout.php

--- server/api/logout.php
+++ server/api/logout.php
@@ -1,18 +1,6 @@
-<?php // logout.php
-
+<?php
 session_start();
-require_once __DIR__ . '/helpers.php';
-
-$_SESSION = [];
-if (ini_get('session.use_cookies')) {
-$params = session_get_cookie_params();
-setcookie(session_name(), '', time() - 42000,
-$params['path'], $params['domain'],
-$params['secure'], $params['httponly']
-);
-}
 session_destroy();
 
-json_response(['message' => 'logged_out']);
+echo json_encode(["success" => true, "message" => "Đăng xuất thành công"]);
 ?>
-
